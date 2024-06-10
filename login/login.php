946e6f8be991fb700e08b92d17e65272219f8cfe
Izra29 <aziznauval042@gmail.com>
2024-06-10 11:04:08 +0700

    Beres

--- login/login.php
+++ login/login.php
@@ -37,8 +37,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="login-container">
       <h1>Login</h1>
       <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="Username" required />
-        <input type="text" name="email" placeholder="Email" required />
+        <input type="text" name="username" placeholder="Username" required autocomplete="off" />
+        <input type="text" name="email" placeholder="Email" required autocomplete="off"/>
         <input type="password" name="password" placeholder="Password" required />
 
         <button type="submit">Login</button>
